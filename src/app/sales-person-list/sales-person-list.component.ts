23e46dce2beec9a90fbf1d9a708e25e13fbe2890
bthank <bthankachan@yahoo.com>
2022-03-12 19:17:36 -0500

    initial commit

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list-bootstrap.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  // create an array of SalesPerson objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("John","Smith", "john.smith@gmail.com",110050),
+    new SalesPerson("Mary","Poler", "mary.poler@gmail.com",51900),
+    new SalesPerson("Alice","Devries", "alice.devries@yahoo.com",73720),
+    new SalesPerson("Tom","Kelly", "tom.kelly@gmail.com",44500)
+  ];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
